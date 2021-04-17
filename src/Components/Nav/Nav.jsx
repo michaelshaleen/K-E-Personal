fd3fd6fba46753d118a07e05fb817e8155b4dddb
michaelshaleen <shaleenmichael@yahoo.com>
2021-04-17 15:56:36 -0500

    drop down

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -34,6 +34,29 @@ function Nav() {
 
   return(
     <>
+    <nav className="navbar">
+      <span className="open-slide">
+        <a href="#">
+          <svg width="30" height="30">
+            <path d="M0,5 30,5" stroke="#000"
+              strokeWidth="5"/>
+            <path d="M0,14 30,14" stroke="#000"
+              strokeWidth="5"/> 
+            <path d="M0,23 30,23" stroke="#000"
+              strokeWidth="5"/>
+          </svg>
+        </a>
+      </span>
+      <ul className="navbar-nav">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+
+
+
+      </ul>
+    </nav>
     
 
 
